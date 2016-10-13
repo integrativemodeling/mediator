58beabf9a894529b6c60a7016304675e4856edea
Ben Webb <ben@salilab.org>
2016-10-13 16:14:39 -0700

    Point to new DOI.

--- sampling/modeling/modeling.py
+++ sampling/modeling/modeling.py
@@ -62,7 +62,7 @@ simo.add_metadata(IMP.pmi.metadata.Citation(
                    'Kornberg RD'],
           doi='10.7554/eLife.08719'))
 simo.add_metadata(IMP.pmi.metadata.Repository(
-          doi="10.5281/zenodo.60814", root="../.."))
+          doi="10.5281/zenodo.160593", root="../.."))
 
 if '--mmcif' in sys.argv:
     # Record the modeling protocol to an mmCIF file
